5358848e851f9ac8a6c0eb5811a3cbeeecca8769
Aaron Brady <aleebrady@gmail.com>
2020-09-01 20:48:16 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,6 +6,11 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :location
       t.string :theme
       t.integer :price
-      t.
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.string :long_description
+    end
+  end
   
 end
