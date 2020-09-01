e42ca0fd94017980608329dbbec450d507b7419d
Aaron Brady <aleebrady@gmail.com>
2020-09-01 20:43:09 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,13 +2,14 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
   
   def change 
     create_table :costume_stores do |t|
-      t.
-      t.
-      t.
-      t.
-      t.
-      
-  
-  
+      t.string :name 
+      t.string :location
+      t.integer :number_of_costumes
+      t.integer :number_of_employees
+      t.boolean :operational?
+      t.string :opening_time 
+      t.string :closing_time
+    end
+  end
   
 end
